750ff5c7cea38747cb4889bbfdf4e580d5dd96f4
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-11 15:06:25 +0100

    101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations - safe first element of a sequence """
+from typing import Any, Mapping, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                        default: Union[T, None] = None) -> Union[Any, T]:
+        """ Return the first element of a list """
+        if key in dct:
+            return dct[key]
+        return default
