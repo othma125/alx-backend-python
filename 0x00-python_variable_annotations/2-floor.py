25f2327822b44ac1361a4322625fa46919eb76fb
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-11 15:28:33 +0100

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Basic annotations - floor """
-from math import floor
+import math
 
 
 def floor(n: float) -> int:
     """ Return the floor of the float """
-    return floor(n)
+    return math.floor(n)
