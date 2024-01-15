209c7ed6adcadf385b08990d1b544ccdab9e7392
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-15 15:34:01 +0100

    2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_runtime(n: int, max_delay: int = 10) -> float:
+def measure_runtime(n: int, max_delay: int) -> float:
     """
 
     Returns:
