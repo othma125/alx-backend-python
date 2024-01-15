4b90da424eb66cfe1deccc3fea50b9827b40224c
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-15 15:00:20 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""[summary]
+"""wait_random module
 """
 import asyncio
 import random
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""wait_n module
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """[summary]
+
+    Args:
+        n (int, optional): [description]. Defaults to 0.
+        max_delay (int, optional): [description]. Defaults to 10.
+
+    Returns:
+        list[float]: [description]
+    """
+    tasks = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    return await asyncio.gather(*tasks)
