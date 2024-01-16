0822104f8dcd248b3d0ba0d347cd087bbdf67048
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-16 10:35:00 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """ Async Generator """
 import asyncio
-import random
-from typing import Generator
+from random import uniform
 
 
 async def async_generator():
     """ Async Generator """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield uniform(0, 10)
