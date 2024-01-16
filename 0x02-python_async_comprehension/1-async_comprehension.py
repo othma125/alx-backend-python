b6f2150cc0a6a94fd5f93d6b62513ceb1dd5a56b
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-16 10:43:21 +0100

    1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Async Comprehensions """
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return the 10 random numbers"""
+    return [i async for i in async_generator()]
