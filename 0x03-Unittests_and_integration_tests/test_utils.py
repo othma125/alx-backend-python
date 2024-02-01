ea822b43350f833ccfb476e923e73aa7f405dce7
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-02-01 15:05:23 +0100

    TestGetJson

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ test utils """
-from unittest import TestCase
-from utils import access_nested_map
+from unittest import TestCase, mock
+from utils import access_nested_map, get_json
+from unittest import mock
 
 
 class TestAccessNestedMap(TestCase):
@@ -23,3 +24,33 @@ class TestAccessNestedMap(TestCase):
         nested_map = {'a': 1}
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path=('a', 'b'))
+
+class TestGetJson(TestCase):
+    """ test get json """
+    def test_get_json(self):
+        """ test get json """
+        test_url = "http://example.com"
+        test_payload = {"payload": True}
+
+        with mock.patch('requests.get') as mock_get:
+            mock_response = mock.Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
+        test_url = "http://holberton.io"
+        test_payload = {"payload": False}
+
+        with mock.patch('requests.get') as mock_get:
+            mock_response = mock.Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
